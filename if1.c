720499a167dcedc5776abbf33f60221c73490362
carvestar <165309143+carvestar@users.noreply.github.com>
2024-10-02 00:31:11 +0900

    Add files via upload

---
+++ if1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+
+	int a;
+	printf("나이를 입력하세요: \n");
+	scanf("%d", &a);
+
+	if (a >= 20 && a <= 39)
+	{
+		printf("청년입니다.\n");
+	}
+
+	else if (a >= 40 && a <= 59)
+	{
+		printf("중장년입니다.\n");
+	}
+	else
+		printf("기타입니다.\n");
+
+	printf("처리를 종료합니다.\n");
+
+	return 0;
+}
